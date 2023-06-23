c39c88521bd000230e35ed42cbb2875716cd26d7
Federico <feulf@users.noreply.github.com>
2023-06-22 22:08:07 -0400

    Update sai.py

--- sai.py
+++ sai.py
@@ -1,13 +1,30 @@
 import os
+import subprocess
 
 import click
 import openai
 
-YOU = "yellow"
-CHATGPT = "green"
-IMPORTANT = "cyan"
-
 openai.api_key = os.environ["OPENAI_API_KEY"]
+
+YELLOW = "yellow"
+GREEN = "green"
+BLACK = "black"
+RED = "red"
+BLUE = "blue"
+MAGENTA = "magenta"
+CYAN = "cyan"
+WHITE = "white"
+BRIGHT_BLACK = "bright_black"
+BRIGHT_RED = "bright_red"
+BRIGHT_GREEN = "bright_green"
+BRIGHT_YELLOW = "bright_yellow"
+BRIGHT_BLUE = "bright_blue"
+BRIGHT_MAGENTA = "bright_magenta"
+BRIGHT_CYAN = "bright_cyan"
+BRIGHT_WHITE = "bright_white"
+
+COMMAND = CYAN
+PROMPT = BRIGHT_BLACK
 
 
 @click.command()
@@ -22,10 +39,9 @@ def cli(question: str):
         messages=[
             {
                 "role": "system",
-                "content": (
-                    "You are a command line tool. "
-                    "If the answer of my question can be a command, answer only the command and nothing else",  # noqa: E501
-                ),
+                "content": "You are a command line tool. "
+                "If the answer of my question can be a command, "
+                "answer only the command and nothing else",
             },
             {"role": "user", "content": question},
         ],
@@ -39,7 +55,15 @@ def cli(question: str):
     elif res[0] == "`" and res[-1] == "`":
         res = res[1:-1]
 
-    click.secho(res, fg=CHATGPT)
+    click.secho("Execute Command: ", fg=PROMPT, nl=False)
+    click.secho(res, fg=COMMAND, nl=False)
+    execute = click.prompt("", default="Y/n", prompt_suffix="")
+
+    if execute == "y" or execute == "Y" or execute == "yes" or execute == "Yes":
+        subprocess.run(res, shell=True)
+
+    else:
+        click.secho("Command not executed.", fg=RED)
 
 
 def main():
